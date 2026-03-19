7d84a2de81d98934694961d4771c4b52106d1abe
Praise Jacob <iampraisejacob@gmail.com>
2026-03-20 02:33:48 +0530

    style: fix indentation of docs

--- cr_vector.h
+++ cr_vector.h
@@ -1,13 +1,13 @@
 /*
-        cr_vector.h - v0.1.0 - dynamically sized array (vector)
+    cr_vector.h - v0.1.0 - dynamically sized array (vector)
 
-        Author:   Praise Jacob <iampraisejacob@gmail.com>
-        Repo:     https://github.com/felix-kyun/shl
+    Author:   Praise Jacob <iampraisejacob@gmail.com>
+    Repo:     https://github.com/felix-kyun/shl
 
-        SPDX-License-Identifier: MIT
-        Copyright (c) 2026 Praise Jacob
+    SPDX-License-Identifier: MIT
+    Copyright (c) 2026 Praise Jacob
 
-        For other informations, see the end of this file.
+    For other informations, see the end of this file.
  */
 
 #ifndef CR_VECTOR_H
@@ -97,15 +97,15 @@ This is a single header library that provides a dynamic array implementation.
 This is part of the Corrosive library.
 
 To use this library, do this in *one* of your source files:
-        #define CR_VECTOR_IMPL
-        #include "cr_vector.h"
+    #define CR_VECTOR_IMPL
+    #include "cr_vector.h"
 
 Table Of Contents
-        Compile time options
-        Documentation
-        Examples
-        License
-        Credits
+    Compile time options
+    Documentation
+    Examples
+    License
+    Credits
 
 Compile time options
         vector_min_capacity <number>
@@ -119,27 +119,27 @@ Examples
         To be added.
 
 MIT License
-        Copyright (c) 2026 Praise Jacob <iampraisejacob@gmail.com>
+    Copyright (c) 2026 Praise Jacob <iampraisejacob@gmail.com>
 
-        Permission is hereby granted, free of charge, to any person obtaining a copy
-        of this software and associated documentation files (the "Software"), to deal
-        in the Software without restriction, including without limitation the rights
-        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-        copies of the Software, and to permit persons to whom the Software is
-        furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-        The above copyright notice and this permission notice shall be included in
-        all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 
 Credits
-        Praise Jacob 	library API/implementation
-        Sean Barret 	built STB which inspired this library
+    Praise Jacob 	library API/implementation
+    Sean Barret 	built STB which inspired this library
  */
