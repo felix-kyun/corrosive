f0cfb39dc947e6bb3e03e497c1fb03eb179b6139
Praise Jacob <iampraisejacob@gmail.com>
2026-04-02 03:57:10 +0530

    feat(cr_log): new library added

---
+++ cr_log.h
@@ -0,0 +1,69 @@
+/*
+    cr_log.h - v0.1.0 - Logging Library
+
+    Author:   Praise Jacob <iampraisejacob@gmail.com>
+    Repo:     https://github.com/felix-kyun/corrosive
+
+    SPDX-License-Identifier: MIT
+    Copyright (c) 2026 Praise Jacob
+
+    For other informations, see the end of this file.
+ */
+
+#ifndef CR_LOG_H
+#define CR_LOG_H
+
+#if defined(CR_LOG_IMPL) || defined(CORROSIVE_IMPLEMENTATION)
+
+#endif // CR_LOG_IMPL
+#endif // CR_LOG_H
+
+/*
+This is a single header library that provides a logging functions.
+This is part of the Corrosive library.
+
+To use this library, do this in *one* of your source files:
+    #define CR_LOG_IMPL
+    #include "cr_log.h"
+
+Table Of Contents
+    Compile time options
+    Documentation
+    Examples
+    License
+    Credits
+
+Compile time options
+        To be added.
+
+Documentation
+        To be added.
+
+Examples
+        To be added.
+
+MIT License
+    Copyright (c) 2026 Praise Jacob <iampraisejacob@gmail.com>
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+Credits
+    Praise Jacob 	library API/implementation
+    Sean Barret 	built STB which inspired this library
+ */
