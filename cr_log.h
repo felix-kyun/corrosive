8674afb5ca43e3dec65e240b5d43139b6d8687fd
Praise Jacob <iampraisejacob@gmail.com>
2026-04-18 17:12:51 +0530

    feat(cr_log): add queue and consumer thread support and non blocking caller function

--- cr_log.h
+++ cr_log.h
@@ -1,5 +1,5 @@
 /*
-    cr_log.h - v0.5.4 - Logging Library
+    cr_log.h - v0.6.0 - Logging Library
 
     Author:   Praise Jacob <iampraisejacob@gmail.com>
     Repo:     https://github.com/felix-kyun/corrosive
@@ -13,12 +13,12 @@
 #ifndef CR_LOG_H
 #define CR_LOG_H
 
-#include <string.h>
 #define _POSIX_C_SOURCE 202405L
-#include <pthread.h>
+#include <stdatomic.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define CR_LOG_LEVEL_TRACE 0
@@ -29,28 +29,47 @@
 #define CR_LOG_LEVEL_FATAL 5
 #define CR_LOG_LEVEL_OFF   6
 
-// CR_LOG_SINK_LIMIT
 #ifndef CR_LOG_SINK_LIMIT
 #define CR_LOG_SINK_LIMIT 8
 #endif
 
-// CR_LOG_BUFFER_SIZE
 #ifndef CR_LOG_BUFFER_SIZE
 #define CR_LOG_BUFFER_SIZE 2048
 #endif
 
-// CR_LOG_PURGE_LEVEL
 #ifndef CR_LOG_PURGE_LEVEL
 #define CR_LOG_PURGE_LEVEL CR_LOG_LEVEL_TRACE
 #endif
 
-// SCOPE
-#ifndef CR_LOG_SCOPE_MAX
-#define CR_LOG_SCOPE_MAX 16
+#ifndef CR_LOG_SINK_FILE_BUFFER
+#define CR_LOG_SINK_FILE_BUFFER 10240
 #endif
+
+#ifndef CR_LOG_SCOPE_MAX_SIZE
+#define CR_LOG_SCOPE_MAX_SIZE 16
+#endif
+
 #ifndef CR_LOG_SCOPE_BUFFER
 #define CR_LOG_SCOPE_BUFFER 256
 #endif
+
+#ifndef CR_LOG_QUEUE_SIZE_POWER
+#define CR_LOG_QUEUE_SIZE_POWER 12
+#endif
+
+#ifndef CR_LOG_QUEUE_ITEM_SIZE
+#define CR_LOG_QUEUE_ITEM_SIZE 512
+#endif
+
+#ifndef CR_LOG_QUEUE_MAX_ENQUEUE_ATTEMPTS
+#define CR_LOG_QUEUE_MAX_ENQUEUE_ATTEMPTS 128
+#endif
+
+#ifndef CR_LOG_QUEUE_MAX_ENQUEUE_BACKOFF
+#define CR_LOG_QUEUE_MAX_ENQUEUE_BACKOFF 256
+#endif
+
+#define CACHE_LINE_SIZE 64
 
 // * compile time purging
 
@@ -109,39 +128,45 @@ void cr_log_set_level(cr_log_level_t level);
 #ifndef CR_LOG_SCOPE_DISABLE
 #define cr_log_scope_push(scope) cr_log__scope_push(scope)
 #define cr_log_scope_pop()       cr_log__scope_pop()
-#else
-#define cr_log_scope_push(scope) ((void)0)
-#define cr_log_scope_pop()       ((void)0)
-#endif
 
 _Thread_local struct {
     int  depth;
     char buffer[CR_LOG_SCOPE_BUFFER];
     int  buffer_length;
-    int  buffer_offsets[CR_LOG_SCOPE_MAX];
+    int  buffer_offsets[CR_LOG_SCOPE_MAX_SIZE];
 } cr_log__scope_stack = { 0 };
 
 void        cr_log__scope_push(const char *scope);
 void        cr_log__scope_pop(void);
 const char *cr_log__scope_get(void);
-
-// * sinks
-#ifndef CR_LOG_SINK_FILE_BUFFER
-#define CR_LOG_SINK_FILE_BUFFER 10240
+#else
+#define cr_log_scope_push(scope) ((void)0)
+#define cr_log_scope_pop()       ((void)0)
 #endif
 
-typedef struct cr_log_sink_event_t {
-    const char     *message;
-    uint8_t         level;
-    struct timespec time_data;
-    const char     *filename;
-    int             line;
-    const char     *function;
-    const char     *scope;
-} cr_log_sink_event_t;
+// clang-format off
+static constexpr size_t buffer_size
+    = CR_LOG_QUEUE_ITEM_SIZE
+    - (sizeof(atomic_size_t)
+    + (sizeof(uint_fast32_t) * 2)
+    + sizeof(struct timespec)
+    + (sizeof(const char *) * 2)
+    + CR_LOG_SCOPE_MAX_SIZE);
+// clang-format on
 
+typedef struct cr_log_item_t {
+    uint8_t         level;
+    uint32_t        line;
+    const char     *filename;
+    const char     *function;
+    struct timespec time;
+    char            scope[CR_LOG_SCOPE_MAX_SIZE];
+    char            buffer[buffer_size];
+} cr_log_item_t;
+
+// * sinks
 typedef struct cr_log_sink_t {
-    void (*process)(void *sink_state, const cr_log_sink_event_t *event);
+    void (*process)(void *sink_state, const cr_log_item_t *item);
     void (*flush)(void *sink_state);
     void (*free)(void *sink_state);
     void *state;
@@ -162,7 +187,7 @@ typedef struct cr_log_sink_file_config_t {
     ssize_t ibuffer_size;
 
     int (*formatter)(
-        char *buf, size_t size, const cr_log_sink_event_t *event, const struct cr_log_sink_file_config_t *config);
+        char *buf, size_t size, const cr_log_item_t *event, const struct cr_log_sink_file_config_t *config);
 } cr_log_sink_file_config_t;
 
 #define cr_log_sink_file(...) cr_log_sink_file_new((struct cr_log_sink_file_config_t) { __VA_ARGS__ })
@@ -174,6 +199,9 @@ cr_log_sink_t cr_log_sink_file_new(struct cr_log_sink_file_config_t config);
 
 #if defined(CR_LOG_IMPL) || defined(CORROSIVE_IMPLEMENTATION)
 
+#include <immintrin.h>
+#include <pthread.h>
+#include <semaphore.h>
 #include <stdarg.h>
 #include <sys/time.h>
 
@@ -184,6 +212,13 @@ cr_log_sink_t cr_log_sink_file_new(struct cr_log_sink_file_config_t config);
         (void)fprintf(stderr, "error(%s:%s:%d)", __FILE__, __func__, __LINE__);                                        \
         (void)fprintf(stderr, (fmt), ##__VA_ARGS__);                                                                   \
     } while (0)
+
+#define atomic_load_relaxed(target)         atomic_load_explicit(target, memory_order_relaxed)
+#define atomic_load_acquire(target)         atomic_load_explicit(target, memory_order_acquire)
+#define atomic_store_relaxed(target, value) atomic_store_explicit(target, value, memory_order_relaxed)
+#define atomic_store_release(target, value) atomic_store_explicit(target, value, memory_order_release)
+#define atomic_cas(target, expected, desired)                                                                          \
+    atomic_compare_exchange_weak_explicit(target, expected, desired, memory_order_relaxed, memory_order_relaxed)
 
 // clang-format off
 static const char* cr_log_reset    = "\x1b[0m";
@@ -205,20 +240,136 @@ static const char* cr_log_level_names[] = {
 };
 // clang-format on
 
+// queue
+constexpr size_t queue_size = 1 << CR_LOG_QUEUE_SIZE_POWER;
+void             queue_consumer(struct cr_log_item_t *item);
+
+struct item {
+    alignas(CACHE_LINE_SIZE) atomic_size_t sequence;
+    struct cr_log_item_t meta;
+};
+
+struct queue {
+    alignas(CACHE_LINE_SIZE) atomic_size_t write;
+    char          pad1[CACHE_LINE_SIZE - sizeof(atomic_size_t)];
+    atomic_size_t read;
+    char          pad2[CACHE_LINE_SIZE - sizeof(atomic_size_t)];
+
+    size_t    mask;
+    pthread_t consumer_thread;
+    sem_t     items;
+
+    struct item buffer[queue_size];
+} queue;
+
+static_assert(sizeof(struct item) == CR_LOG_QUEUE_ITEM_SIZE, "item too large");
+static_assert(alignof(struct item) == CACHE_LINE_SIZE, "alignment broken");
+
+int
+enqueue_(struct cr_log_item_t *meta)
+{
+    for (;;) {
+        size_t write_pos = atomic_load_relaxed(&queue.write);
+        size_t idx       = write_pos & queue.mask;
+        size_t seq       = atomic_load_relaxed(&queue.buffer[idx].sequence);
+
+        // check availablity
+        if (write_pos - seq == 0) {
+            // available, try cas
+            if (atomic_cas(&queue.write, &write_pos, write_pos + 1)) {
+                // claimed
+                queue.buffer[idx].meta = *meta;
+                atomic_store_release(&queue.buffer[idx].sequence, write_pos + 1);
+                sem_post(&queue.items);
+                return 0;
+            }
+            // cas failed, retry
+        } else {
+            // full or stale
+            return -1;
+        }
+    }
+
+    return -1;
+}
+
+int
+enqueue(struct cr_log_item_t meta)
+{
+    int backoff = 1;
+
+    for (int i = 0; i < CR_LOG_QUEUE_MAX_ENQUEUE_ATTEMPTS; i++) {
+        int ret = enqueue_(&meta);
+        if (ret == 0) {
+            return 0;
+        } else if (ret == -1) {
+            // spin and retry
+            // exponential backoff
+            for (int j = 0; j < backoff; j++) {
+                _mm_pause();
+            }
+            backoff = (backoff < CR_LOG_QUEUE_MAX_ENQUEUE_BACKOFF) ? backoff << 1 : CR_LOG_QUEUE_MAX_ENQUEUE_BACKOFF;
+        }
+    }
+
+    // drop
+    return -1;
+}
+
+int
+try_dequeue(void)
+{
+    size_t read_pos = atomic_load_relaxed(&queue.read);
+    size_t idx      = read_pos & queue.mask;
+    size_t seq      = atomic_load_acquire(&queue.buffer[idx].sequence);
+    long   diff     = (long)seq - (long)(read_pos + 1);
+
+    if (diff == 0) {
+        // consume
+        queue_consumer(&queue.buffer[idx].meta);
+
+        // release
+        atomic_store_relaxed(&queue.buffer[idx].sequence, read_pos + queue_size);
+        atomic_store_relaxed(&queue.read, read_pos + 1);
+
+        return 0;
+    }
+
+    return -1;
+}
+
+void *
+dequeue([[maybe_unused]] void *arg)
+{
+    for (;;) {
+        sem_wait(&queue.items);
+        while (try_dequeue() == 0) {
+            if (sem_trywait(&queue.items) != 0) {
+                break;
+            }
+        }
+    }
+
+    return NULL;
+}
+
 static struct {
-    pthread_mutex_t lock;
-    cr_log_level_t  level;
-    cr_log_sink_t   sinks[CR_LOG_SINK_LIMIT];
-    size_t          sink_count;
-    char           *buffer;
+    cr_log_level_t level;
+    cr_log_sink_t  sinks[CR_LOG_SINK_LIMIT];
+    size_t         sink_count;
 } logger_state = { 0 };
 
 void
 cr_log_init(void)
 {
-    logger_state.level  = CR_LOG_LEVEL_INFO;
-    logger_state.buffer = (char *)malloc(CR_LOG_BUFFER_SIZE);
-    pthread_mutex_init(&logger_state.lock, NULL);
+    logger_state.level = CR_LOG_LEVEL_INFO;
+    for (size_t i = 0; i < queue_size; i++) {
+        atomic_init(&queue.buffer[i].sequence, i);
+    }
+    queue.mask = queue_size - 1;
+    atomic_init(&queue.read, 0);
+    atomic_init(&queue.write, 0);
+    pthread_create(&queue.consumer_thread, NULL, dequeue, NULL);
 }
 
 void
@@ -238,10 +389,18 @@ cr_log_flush(void)
 void
 cr_log_free(void)
 {
+    pthread_join(queue.consumer_thread, NULL);
     for (int i = 0; i < (int)logger_state.sink_count; i++) {
         logger_state.sinks[i].free(logger_state.sinks[i].state);
     }
-    free(logger_state.buffer);
+}
+
+void
+queue_consumer(struct cr_log_item_t *item)
+{
+    for (int i = 0; i < (int)logger_state.sink_count; i++) {
+        logger_state.sinks[i].process(logger_state.sinks[i].state, item);
+    }
 }
 
 void
@@ -252,30 +411,28 @@ cr_log(cr_log_level_t level, const char *file, int line, const char *func, const
         return;
     }
 
-    cr_log_sink_event_t event = {
-        .message   = logger_state.buffer,
+    // clang-format off
+    cr_log_item_t event = {
+        // .message   = logger_state.buffer,
         .level     = level,
-        .time_data = { 0 },
+        .time = { 0 },
         .filename  = file,
-        .line      = line,
+        .line      = (uint32_t)line,
         .function  = func,
-        .scope     = cr_log__scope_get(),
     };
+    // clang-format on
 
-    clock_gettime(CLOCK_REALTIME_COARSE, &event.time_data);
+    strncpy(event.scope, cr_log__scope_get(), CR_LOG_SCOPE_MAX_SIZE - 1);
+    event.scope[CR_LOG_SCOPE_MAX_SIZE - 1] = '\0';
 
-    pthread_mutex_lock(&logger_state.lock);
+    clock_gettime(CLOCK_REALTIME_COARSE, &event.time);
 
     va_list args;
     va_start(args, fmt);
-    (void)vsnprintf(logger_state.buffer, CR_LOG_BUFFER_SIZE, fmt, args);
+    (void)vsnprintf(event.buffer, buffer_size, fmt, args);
     va_end(args);
 
-    for (int i = 0; i < (int)logger_state.sink_count; i++) {
-        logger_state.sinks[i].process(logger_state.sinks[i].state, &event);
-    }
-
-    pthread_mutex_unlock(&logger_state.lock);
+    enqueue(event);
 }
 
 // * Scope
@@ -283,7 +440,7 @@ void
 cr_log__scope_push(const char *scope)
 {
     auto stack = &cr_log__scope_stack;
-    if (likely(stack->depth < CR_LOG_SCOPE_MAX)) {
+    if (likely(stack->depth < CR_LOG_SCOPE_MAX_SIZE)) {
         // save reset point
         stack->buffer_offsets[stack->depth] = stack->buffer_length;
 
@@ -344,9 +501,9 @@ typedef struct cr_log_sink_file_state_t {
 } cr_log_sink_file_state_t;
 
 void cr_log__sink_file_flush(void *sink_state);
-void cr_log__sink_file_process(void *state, const cr_log_sink_event_t *event);
+void cr_log__sink_file_process(void *state, const cr_log_item_t *item);
 int  cr_log__sink_file_format(
-    char *buffer, size_t size, const cr_log_sink_event_t *event, const struct cr_log_sink_file_config_t *config);
+    char *buffer, size_t size, const cr_log_item_t *item, const struct cr_log_sink_file_config_t *config);
 void cr_log__sink_file_free(void *sink_state);
 
 cr_log_sink_t
@@ -409,7 +566,7 @@ cr_log_sink_file_new(struct cr_log_sink_file_config_t config)
 
 int
 cr_log__sink_file_format(
-    char *buf, size_t size, const cr_log_sink_event_t *event, const struct cr_log_sink_file_config_t *config)
+    char *buf, size_t size, const cr_log_item_t *item, const struct cr_log_sink_file_config_t *config)
 {
     int color = (int)config->color;
     return snprintf(
@@ -417,40 +574,40 @@ cr_log__sink_file_format(
         size,
         "[%ld.%ld] [%s%s%s] [%s] [%s:%d %s] ",
         // time
-        event->time_data.tv_sec,
-        event->time_data.tv_nsec,
+        item->time.tv_sec,
+        item->time.tv_nsec,
         // level
-        color ? cr_log_colors[event->level] : "",
-        cr_log_level_names[event->level],
+        color ? cr_log_colors[item->level] : "",
+        cr_log_level_names[item->level],
         color ? cr_log_reset : "",
         // scope
-        event->scope[0] == '\0' ? " " : event->scope,
+        item->scope[0] == '\0' ? " " : item->scope,
         // location
-        event->filename,
-        event->line,
-        event->function);
+        item->filename,
+        item->line,
+        item->function);
 }
 
 void
-cr_log__sink_file_process(void *sink_state, const cr_log_sink_event_t *event)
+cr_log__sink_file_process(void *sink_state, const cr_log_item_t *item)
 {
     cr_log_sink_file_state_t *state = sink_state;
-    if (event->level < state->config.level) {
+    if (item->level < state->config.level) {
         return;
     }
 
     char buf[CR_LOG_SINK_FILE_FORMAT_MAX];
-    state->config.formatter(buf, sizeof(buf), event, &state->config);
+    state->config.formatter(buf, sizeof(buf), item, &state->config);
 
     if (state->buffer_size == 0) {
         // bypass buffer, write directly
-        (void)fprintf(state->file_stream, "%s%s\n", buf, event->message);
+        (void)fprintf(state->file_stream, "%s%s\n", buf, item->buffer);
         return;
     }
 
     // try
     int offset = snprintf(
-        state->buffer + state->offset, state->buffer_size - (size_t)state->offset, "%s%s\n", buf, event->message);
+        state->buffer + state->offset, state->buffer_size - (size_t)state->offset, "%s%s\n", buf, item->buffer);
 
     if (offset < 0) {
         perror("(snprintf) failed to append to file sink buffer");
@@ -460,7 +617,7 @@ cr_log__sink_file_process(void *sink_state, const cr_log_sink_event_t *event)
     if (offset > (int)state->buffer_size) {
         // larger than buffer, write through
         cr_log__sink_file_flush(sink_state);
-        (void)fprintf(state->file_stream, "%s%s\n", buf, event->message);
+        (void)fprintf(state->file_stream, "%s%s\n", buf, item->buffer);
         return;
     }
 
@@ -468,7 +625,7 @@ cr_log__sink_file_process(void *sink_state, const cr_log_sink_event_t *event)
         // buffer overflow, flush and retry
         cr_log__sink_file_flush(sink_state);
         int ret = snprintf(
-            state->buffer + state->offset, state->buffer_size - (size_t)state->offset, "%s%s\n", buf, event->message);
+            state->buffer + state->offset, state->buffer_size - (size_t)state->offset, "%s%s\n", buf, item->buffer);
 
         if (ret < 0) {
             perror("(snprintf) failed to append to file sink buffer");
