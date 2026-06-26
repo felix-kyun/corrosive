934c711951ea08c925f4c73db7ca98c5fc1db754
Praise Jacob <iampraisejacob@gmail.com>
2026-06-27 03:49:16 +0530

    chore(cr_log): version bump

--- cr_log.h
+++ cr_log.h
@@ -1,5 +1,5 @@
 /*
- * cr_log.h - v0.8.0 - Logging Library
+ * cr_log.h - v0.9.0 - Logging Library
  *
  * Author:   Praise Jacob <iampraisejacob@gmail.com>
  * Repo:     https://github.com/felix-kyun/corrosive
